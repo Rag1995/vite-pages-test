42cbd03ac595f3026e921656309f66cbb7b3d761
Rag1995 <taipeiyyy@gmail.com>
2022-08-29 14:46:53 +0800

    deploy

---
+++ assets/_id_.8938e05c.js
@@ -0,0 +1,1 @@
+import{d as t,o as n,c as o,t as a}from"./index.f73769bf.js";const l=t({__name:"[id]",props:{id:null},setup(e){return(r,s)=>(n(),o("h1",null,"USER ID: "+a(e.id),1))}});export{l as default};
